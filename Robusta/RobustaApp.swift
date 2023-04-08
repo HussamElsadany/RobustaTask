053388c29251ea6bf322ac8dc9e47d74f23a6f43
HussamElsadany <hussam.elsadany@gmail.com>
2023-04-08 07:12:16 +0200

    Initial Commit

---
+++ Robusta/RobustaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobustaApp.swift
+//  Robusta
+//
+//  Created by Hussam Elsadany on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RobustaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
